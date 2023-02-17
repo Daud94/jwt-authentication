3b458869f7949c41682eb780f5c416bb992ba2b1
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2023-02-17 18:02:35 +0100

    refractoring

--- app.js
+++ app.js
@@ -3,34 +3,9 @@ const bodyParser = require("body-parser")
 const dotenv = require('dotenv')
 const Sequelize = require("sequelize");
 const {DataTypes} = require("sequelize");
-const jwt = require('jsonwebtoken');
-
-
-
-
+const {generateAccessToken,authenticateToken} = require('./jwt-authentication')
 
 dotenv.config()
-
-function generateAccessToken(username) {
-    return jwt.sign(username, process.env.JWT);
-}
-
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-
-    if (token == null) return res.sendStatus(401)
-
-    jwt.verify(token, process.env.JWT, (err, user) => {
-        console.log(err)
-
-        if (err) return res.sendStatus(403)
-
-        req.user = user
-
-        next()
-    })
-}
 
 // mysql database connection
 const sequelize = new Sequelize(
